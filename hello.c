bef91b1dcf13a9ab11f4849c6c5c175cd99b4041
Rollaro <69832944+Rollaro@users.noreply.github.com>
2020-08-19 11:10:58 -0500

    Update hello.c
    Added my name and major as per the request

--- hello.c
+++ hello.c
@@ -11,6 +11,8 @@
  int main(int argc, char **agrv) {
      printf("Lyndsay Gunther!!!\n");
      printf("Computer science\n");
+     printf("Thomas Mitchell\n");
+     printf("Computer Science\n);
   
      return 0;
  }
